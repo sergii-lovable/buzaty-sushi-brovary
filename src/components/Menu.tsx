a95ff9374b3e0b19cb12c744927fad2bac809fc6
ukrtrendshop-ai <ukrtrendshop@gmail.com>
2025-10-14 14:13:00 +0300

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -454,7 +454,7 @@ const menuItems: MenuItemType[] = [
     name: "Макі лосось слабосолений",
     description: "Лосось с/с, рис, норі",
     price: 109,
-    image: "/images/sushi-roll.jpg",
+    image: "/images/Макі лосось слабосолений.png",
     category: "futo-maki"
   },
   {
