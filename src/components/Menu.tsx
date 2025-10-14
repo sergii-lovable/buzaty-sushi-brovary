37b5fec9fa740baca42cfdeab97d5a1ed00a1251
ukrtrendshop-ai <ukrtrendshop@gmail.com>
2025-10-14 14:17:54 +0300

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -510,7 +510,7 @@ const menuItems: MenuItemType[] = [
     name: "Біг рол з тунцем спайсі",
     description: "Тунець спайсі, салат, помідор, майонез, рис, норі",
     price: 209,
-    image: "/images/sushi-roll.jpg",
+    image: "/images/Біг рол з тунцем спайсі.png",
     category: "futo-maki"
   },
   {
