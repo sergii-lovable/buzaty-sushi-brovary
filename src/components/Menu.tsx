3d89433477f8fd3e521a882c56fd6df492ade16e
ukrtrendshop-ai <ukrtrendshop@gmail.com>
2025-10-14 17:18:18 +0300

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -502,7 +502,7 @@ const menuItems: MenuItemType[] = [
     name: "Норвезький",
     description: "Креветка, огірок, крем-сир, ікра масаго, рис, норі",
     price: 229,
-    image: "/images/Норвезький.png",
+    image: "/images/Норвезький.jpg",
     category: "futo-maki"
   },
   {
